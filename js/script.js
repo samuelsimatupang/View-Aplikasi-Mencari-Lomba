2282a3d87070d1c8170f8e131a77693d6aa3ef3e
Samuel Simatupang <74776325+samuelsimatupang@users.noreply.github.com>
2022-06-05 13:17:16 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiperEvent", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
